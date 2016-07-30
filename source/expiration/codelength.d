88273626ba0c8a67ebe4eb539b1b31355fea5651
Khaled Nassar <kmhnassar@gmail.com>
2016-07-30 14:01:37 +0200

    Added codelength-based expiration policy. Closes #11.

---
+++ source/expiration/codelength.d
@@ -0,0 +1,48 @@
+import vibe.data.json;
+import std.datetime;
+import std.typecons;
+import std.array;
+import std.format;
+import expirationpolicy;
+import database;
+import util;
+
+class CodeLengthExpirationPolicy  : ExpirationPolicy
+{
+    private:
+        Nullable!SysTime calculateExpirationTime(ulong codeLength)
+        {
+            Nullable!SysTime expTime = Clock.currTime;
+            auto expirationHours = (codeLength * 26) - 20;
+            expTime.roll!"hours"(expirationHours);
+            
+            return expTime;
+        }
+        
+    protected:
+        string[string] settings;
+        bool useKey;
+
+    public:
+        this(string[string] settings)
+        {
+            this.settings = settings;
+            this.useKey = ("bypassKey" in settings) !is null;
+        }
+
+        bool initExpirationDateTime(Json json, ref Entry entry)
+        {
+            if (useKey && json["bypassKey"].to!string == (settings["bypassKey"]))
+            {
+                return false;
+            }
+            auto codeLength = entry.shortCode.length;
+            entry.expireDateTime = calculateExpirationTime(codeLength);
+            return true;
+        }
+
+        bool updateExpirationDateTime(ref Entry entry)
+        {
+            return false;
+        }
+}
